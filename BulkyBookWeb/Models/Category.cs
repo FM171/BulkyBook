904486e38dce812582ce6344a8389c630a265d42
Faranak <Owner@DESKTOP-SK39C64>
2022-08-20 19:59:23 +0100

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category //public class can called by other fuctions
+    {
+        [Key] //This  is data attribute that turns id into primary key like sql
+        public int Id { get; set; }
+        
+        [Required] //This turns Key into required field
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now; 
+    }
+}
